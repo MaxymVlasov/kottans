1d02bb70bcc60c1bee6d35da54024cef72545fad
Maxym Vlasov <MaxymVlasov@users.noreply.github.com>
2016-04-08 22:39:32 +0300

    Add boundBox (point1, point2) Building minimum bounding box Building a rectangle, take a segment as the long diagonal of rectangle point1 - start, point2 - end of segment How it works:  https://goo.gl/VIQqvZ 35.2 (p.796-797) or https://goo.gl/nWosBl

--- intersections/solution.js
+++ intersections/solution.js
@@ -22,6 +22,25 @@ function intersects (fig1, fig2) {
     return {x: x, y: y}
   }
 
+  function boundBox (point1, point2) {
+    var b = {xmax: null, ymax: null, xmin: null, ymin: null}
+    if (point1.x > point2.x) {
+      b.xmax = point1.x
+      b.xmin = point2.x
+    } else {
+      b.xmax = point2.x
+      b.xmin = point1.x
+    }
+    if (point1.y > point2.y) {
+      b.ymax = point1.y
+      b.ymin = point2.y
+    } else {
+      b.ymax = point2.y
+      b.ymin = point1.y
+    }
+    return {xmax: b.xmax, xmin: b.xmin, ymax: b.ymax, ymin: b.ymin}
+  }
+
   return [
     [
       { x: 60,  y: 240 },
